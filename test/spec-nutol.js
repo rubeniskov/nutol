e2e4d7d6e01cabcf1b780c850b28863e709d51a1
Rubén López Gómez <loslopez1@icloud.com>
2016-05-17 10:28:10 +0200

    add:  first unit test

---
+++ test/spec-nutol.js
@@ -0,0 +1,23 @@
+var expect = require("chai").expect;
+var nutol = require("..");
+
+describe('Nutol', function() {
+
+    describe('isObject', function() {
+        it('should return a true', function() {
+            expect(nutol.isObject({})).to.be.true;
+        });
+    });
+
+    describe('isPlainObject', function() {
+        it('should return a true', function() {
+            expect(nutol.isPlainObject({})).to.be.true;
+        });
+    });
+
+    describe('isArray', function() {
+        it('should return a true', function() {
+            expect(nutol.isArray([])).to.be.true;
+        });
+    });
+});
