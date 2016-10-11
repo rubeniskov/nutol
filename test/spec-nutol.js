3db549fe9038b5672cd25e95b51a46fde79c2bea
Rubén López Gómez <loslopez1@icloud.com>
2016-10-11 16:47:09 +0200

    update: unit test

--- test/spec-nutol.js
+++ test/spec-nutol.js
@@ -64,4 +64,25 @@ describe('Nutol', function() {
 
         });
     });
+
+    describe('objects', function() {
+        it('should return property keys', function() {
+            var obj = {
+                'foo': 'bar'
+            };
+
+            expect(obj).to.have.property('foo');
+            expect(nutol.keys(obj)).that.is.an('array');
+            expect(nutol.keys(obj)).to.include.members(['foo']);
+        });
+
+        it('should return property values', function() {
+            var obj = {
+                'foo': 'bar'
+            };
+            expect(obj).to.have.property('foo');
+            expect(nutol.values(obj)).that.is.an('array');
+            expect(nutol.values(obj)).to.include.members(['bar']);
+        });
+    });
 });
